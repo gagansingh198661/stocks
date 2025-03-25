1f38eac6478f8831e41095d088a7d38d6f40ad57
Gagan <gagan_singh198661@yahoo.com>
2025-03-25 17:07:40 +0530

    Added BuyMoreRule

---
+++ src/main/java/com/example/stocks/rules/BuyMoreRule.java
@@ -0,0 +1,50 @@
+package com.example.stocks.rules;
+
+import com.example.stocks.dto.*;
+import com.example.stocks.entity.Alert;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BuyMoreRule implements Rule{
+    @Override
+    public InfoDTO applyRule(InfoDTO infoDTO) {
+        List<Alert> alerts = infoDTO.getAlerts();
+        List<AlertDTO> alertDTOList = infoDTO.getAlertDTOList();
+        if (alertDTOList==null){
+            alertDTOList=new LinkedList<>();
+        }
+        BigDecimal currentPrice = infoDTO.getStock().getCurrentPrice();
+        BigDecimal boughtPrice = infoDTO.getStock().getBoughtPrice();
+        if (alerts!=null&&alerts.size()!=0){
+            for (Alert alert:
+                 alerts) {
+                if (alert.getAlertType().equals(AlertType.BUYMORE.name())){
+                    int percent = alert.getPercent();
+                    if(belowPercent(boughtPrice,currentPrice,percent)){
+                        AlertDTO alertDTO = new AlertDTO("Stock : "+ infoDTO.getStock().getStockSymbol() +" has declined by "+percent+"%", boughtPrice.toPlainString(),currentPrice.toString(), Action.HOLD, Type.NORMAL);
+                        alertDTOList.add(alertDTO);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean belowPercent(BigDecimal bought, BigDecimal current, int percent) {
+        if(bought.compareTo(current)>0){
+            BigDecimal result = bought.subtract(current);
+            result = result.multiply(new BigDecimal(100));
+            result = result.abs();
+            result = result.divide(bought,0, RoundingMode.HALF_UP);
+            if(result.compareTo(new BigDecimal(percent))>=0){
+                return  true;
+            }else {
+                return false;
+            }
+        }
+        return false;
+    }
+}
